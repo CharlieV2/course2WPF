c4ae4413896f32e44f66f557f93aaee0389881e5
charlierein <charlierein@mail.ru>
2020-03-25 19:01:00 +0300

    sync

---
+++ wpftest/HelpClasses/InfoBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpftest.HelpClasses
+{
+    public static class InfoBase
+    {
+        #region InfoBase
+        public static string[] countryArray = { "Japan", "Germany", "Russia", "French", "Gr. Britain", "USA", "Canada" };
+
+        public static string[] JapanNameArray = { "Ai", "Aiko", "Akako", "Akane", "Akemi", "Aki", "Chika", "Chikako ", "Cho ", "Dai ", "Hoshi ", "Akeno ", "Akiyama ", "Fudo ", "Naruto ", "Sasuke ", "Botan ", "Daiki ", "Haru ", "Jiro ", "Kanaye ", "Kano ", "Kazuo ", "Rafu ", "Nikk" };
+        public static string[] JapanSurnameArray = { "Sato", "Sudjuki", "Ito", "Yamat", "Kato", "Sasaki", "Uchiha", "Uzumaki", "Yamaguti", "Hayasi", "Abe", "Goto", "Ocade", "Ono", "Takano", "Seke", "Homma" };
+
+        public static string[] GermanyNameArray = { "Adolf", "Ben", "Paul", "Jonas", "Elias", "Leon", "Finn", "Fynn", "Noah", "Luis", "Luke", "Felix", "Luca", "Maximilian", "Henry", "Max", "Oskar", "Emil", "Liam", "Jacob", "Moritz", "Anton", "Mia", "Emma", "Sofia", "Lina", "Mila", "Amelie", "Lilly" };
+        public static string[] GermanySurnameArray = { "Miller", "Scholz", "Tailor", "Weaver", "Baker", "Koch", "Bauer", "Richter", "Klein", "Wolf", "Schwarz", "Lange", "Werner", "Krause", "Maier", "Jung", "Hneitler", "Friedrich", "Frank", "Roth", "Beck", "Lorenz", "Franke" };
+
+        public static string[] RussianNameArray = { "Alexander", "Alexey", "Anton", "Andrei", "Artur", "Bogdan", "Boris", "Valeriy", "Vitaliy", "Gleb", "Grigoriy", "Denis", "Zahar", "Ivan", "Ignat", "Igor", "Kirill", "Oksana", "Lara", "Olesya", "Yuliya" };
+        public static string[] RussianSurnameArray = { "Smirnov", "Popov", "Kuznecov", "Sobolev", "Maslov", "Moiseev", "Shukin", "Sidorov", "Egorov", "Savin", "Guseev", "Titon", "Shilov", "Mishin", "Phokin", "Nosov" };
+
+        public static string[] FrenchNameArray = { "Arno", "Jean", "Michel", "Alain", "Patrick", "Nicolas", "Christian", "Daniel", "Pierre", "Aim", "Bles", "Gustav", "Camill", "Rafael", "Natali", "Lulu", "Lor", "Clar", "Adel", "Anet", "Agata" };
+        public static string[] FrenchSurnameArray = { "Azule", "Arno", "Arkur", "Arias", "Buzho", "Bosh", "Benua", "Zharr", "Dumash", "Labule", "Ledu", "Mereli", "Peti", "Reber" };
+
+        public static string[] EnNameArray = { "Amelia", "Olivia", "Oliver", "Jack", "Harry", "Jacob", "Charlie", "Thomas", "Oscar", "William", "James", "George", "Alfie", "Leo", "Henry", "Max", "Jake", "Lola", "Holly", "Lilly", "Rosie" };
+        public static string[] EnSurnameArray = { "Abramson", "Adderiy", "Howard", "Holiday", "Jeff", "Jenkin", "Batton", "Alsopp", "Andrews", "Arnold", "Kelly", "Page", "Day", "Derrick", "Ralphs", "Ryder", "Young", "Haig" };
+
+        public static string[] CanadaNameArray = { "Carter", "Daniel", "Felix", "Gabriel", "Jacob", "Antoine", "Alexis", "Liam", "Mason", "Noah", "Olivier", "Owen", "Lucas", "Tyler", "William", "Hailey", "Jessica", "Lea" };
+        public static string[] CanadaSurnameArray = { "Abramson", "Adderiy", "Howard", "Holiday", "Jeff", "Jenkin", "Batton", "Alsopp", "Andrews", "Arnold", "Kelly", "Page", "Day", "Derrick", "Ralphs", "Ryder", "Young", "Haig" };
+
+        public static string[] Sport = { "Biathlon", "Bobsleigh", "Curling", "Snowboard", "Ski race", "Skiing"};
+        #endregion
+    }
+}
---
+++ wpftest/HelpClasses/Sportsmen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpftest.HelpClasses
+{
+    public static class Sportsmen
+    {
+        public static string Country;
+        public static string Name;
+        public static string Surname;
+        public static string Sport;
+        public static int Age;
+        public static int Gold;
+        public static int Silver;
+        public static int Bronze;
+
+        public static void Generate()
+        {
+            
+        }
+    }
+}
